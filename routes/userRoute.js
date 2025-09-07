a1c2dddb2b656433966c5100ad8c542850baab6f
Mulraj Gala <mulrajgala6@gmail.com>
2025-09-07 18:02:51 +0530

    updated new routes to controller and route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,14 @@
 import express from "express";
 const app = express()
 const router = express.Router()
+import {AR} from "../controllers/authController.js";
 import {R} from "../controllers/siteController.js";
 
 router.get("/user/get",R.getUser);
 router.post("/user/create",R.createUser);
-router.post("/user/register",R.register);
-router.post("/user/login",R.login);
+router.post("/user/register",AR.register);
+router.post("/user/refresh",AR.refresh);
+router.post("/user/login",AR.login);
 router.put("/user/update/:id",R.updateUser);
 router.delete("/user/delete/:id",R.deleteUser);
 
